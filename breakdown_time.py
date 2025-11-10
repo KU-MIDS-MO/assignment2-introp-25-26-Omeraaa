d0010d265b374662105835bf8772a0941bf2ba79
Ömer Boruç <omerboruc@Omer-MacBook-Pro.local>
2025-11-10 16:15:01 +0100

    Assignment 2 Completed

--- breakdown_time.py
+++ breakdown_time.py
@@ -1,4 +1,26 @@
 def breakdown_time(seconds):
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+
+    if not isinstance(seconds, int) or seconds < 0:
+        return -1
+    
+    if seconds == 0:
+        return {}
+    
+
+    hours = seconds // 3600
+    remainingseconds = seconds % 3600
+    minutes = remainingseconds // 60
+    realremainingseconds = remainingseconds % 60
+    
+    finaldict = {} 
+    
+    if hours > 0:
+        finaldict[3600] = hours
+        
+    if minutes > 0:
+        finaldict[60] = minutes
+        
+    if realremainingseconds > 0:
+        finaldict[1] = realremainingseconds
+    
+    return finaldict
--- swap_ends.py
+++ swap_ends.py
@@ -1,4 +1,21 @@
 def swap_ends(L, k):
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+    
+    if k <= 0:
+        return (L.copy(), 0)
+    
+    listlenght = len(L)
+    
+    if listlenght == 0 or k > listlenght / 2:
+        return (L.copy(), 0)
+    
+    partA = L[0:k]
+    
+    partB = L[k:-k]
+    
+    partC = L[-k:]
+    
+    newlist = partC + partB + partA
+    
+    numswaps = k
+    
+    return (newlist, numswaps)
